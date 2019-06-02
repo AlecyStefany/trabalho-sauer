86ede239267bc40c8c5dea2c5a29c50c8251851e
AlecyStefany <50998111+AlecyStefany@users.noreply.github.com>
2019-06-02 15:17:39 -0300

    Update login.php

--- login.php
+++ login.php
@@ -1,25 +1,32 @@
 <?php 
 include ("conexaobanco.php");
 include("funcoes.php");
+  session_start();
   $login = $_POST["login"];
 
   $entrar = $_POST["entrar"];
   $pass = md5($_POST["pass"]);
    if (isset($entrar)) {
-    $query_select = "SELECT login, pass FROM usuario WHERE login  = '$login' and pass ='$pass'";       
-    $resultado=conecta($maquina,$usuario,$senha,$banco,$query_select);
-      if (mysql_num_rows($resultado)<=0){
-        echo"<script language='javascript' type='text/javascript'>
+    $con = mysql_connect("127.0.0.1", "root", "root") or die
+ ("Sem conexão com o servidor");
+
+
+$query_select = "SELECT login, pass FROM usuario WHERE login  = '$login' and pass ='$pass'";       
+$resultado=conecta($maquina,$usuario,$senha,$banco,$query_select);
+  if (mysql_num_rows($resultado)<=0){
+     unset ($_SESSION['login']);
+  unset ($_SESSION['pass']);
+  echo"<script language='javascript' type='text/javascript'>
         alert('Login e/ou senha incorretos');window.location
         .href='index.html';</script>";
-        die();
-      }else{
-        setcookie("login",$login);
-        header("Location:financa.html");
-      }
-  }
-  
-  
+        die();}
+  else{
+{
+$_SESSION['login'] = $login;
+$_SESSION['pass'] = $pass;
+header('location:financa.html');
+}
 
+ 
  
 ?>
